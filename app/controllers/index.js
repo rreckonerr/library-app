b0927a5ebde272f9c2b1b035740385274c86f957
rreckonerr <rreckonerr@gmail.com>
2018-05-29 14:04:45 +0700

    added email input box

---
+++ app/controllers/index.js
@@ -0,0 +1,22 @@
+import { not, match } from '@ember/object/computed'
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+	emailAddress: '',
+	responseMessage: '',
+	
+	isValid: match('emailAddress', /^.+@.+\..+$/),
+	isDisabled: not('isValid'),
+
+	actions: {
+
+		saveInvitation() {
+			alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+		this.set('responseMessage', `Thank you! We've just saved your email address:
+			${this.get('emailAddress')}`);
+		this.set('emailAddress', '');
+		}
+	}
+
+});
